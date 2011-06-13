4888eaf087f629861fa1aac6b1b40836c7ac658b
Joachim Haagen Skeie <joachim@haagen.name>
2011-06-13 13:39:49 +0200

    Adding files

--- jwirereaplay.View/build_dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SproutCore build tools
-rm -rf ../jwirereplay.Web/src/main/webapp/static
-rm -rf ./tmp
-/usr/bin/sc-build --build jwirereplay
-cp -R ./tmp/build/static ../jwirereplay.Web/src/main/webapp/static
-
