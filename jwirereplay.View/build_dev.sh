069d82a2c3cb5fd9aec0d5a0b058cdc5099c012b
Joachim Haagen Skeie <joachim@haagen.name>
2011-09-09 23:25:47 +0200

    Upgraded Jetty to version 8. Added redirect into the sprout core app.

--- jwirereplay.View/build_dev.sh
+++ jwirereplay.View/build_dev.sh
@@ -3,6 +3,6 @@
 #SproutCore build tools
 rm -rf ../jwirereplay.Web/src/main/webapp/static
 rm -rf ./tmp
-/usr/bin/sc-build --build jwirereplay
+sc-build --build jwirereplay
 cp -R ./tmp/build/static ../jwirereplay.Web/src/main/webapp/static
 
